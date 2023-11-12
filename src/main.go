c55827dcc19413d840736750cc63565ad62b3ee3
JakubSw <jakub.swastek@wp.pl>
2023-11-12 18:08:52 +0000

    Trigger pipeline

--- src/main.go
+++ src/main.go
@@ -4,14 +4,10 @@ import "net/http"
 
 func main() {
 
-	// Create a new request multiplexer
-	// Take incoming requests and dispatch them to the matching handlers
 	mux := http.NewServeMux()
 
-	// Register the routes and handlers
 	mux.Handle("/", &homeHandler{})
 
-	// Run the server
 	http.ListenAndServe(":8080", mux)
 }
 
